d6602b4d1790ba53969a340cf9967ec7b53ddeb1
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-11-10 19:30:48 +0900

    init: setup

---
+++ src/main/java/com/heartauction/HeartauctionApplication.java
@@ -0,0 +1,13 @@
+package com.heartauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartauctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartauctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heartauction/HeartauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartauctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
